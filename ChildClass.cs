4ea085b714d5b7b1e8619518d1a90202ac2acf5a
NikolayOkhlopovskiy <okhlopovskiynikolay@yahoo.com>
2025-04-16 23:09:54 +0300

    Add files via upload

---
+++ ChildClass.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp36._2
+{
+    class ChildClass
+    {
+    }
+}
